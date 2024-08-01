2ee6c2ea2f2704ce1615c83edb35b845b294b235
Nico Gallinal <nicoabie@gmail.com>
2024-08-01 18:18:26 -0300

    add restrictionsToBits util

--- utils.test.ts
+++ utils.test.ts
@@ -1,6 +1,6 @@
 import { describe, expect, test } from "bun:test";
 
-import { datetimeToISO8601 } from "./utils";
+import { datetimeToISO8601, restrictionsToBits } from "./utils";
 
 describe("datetimeToISO8601", () => {
 	// ideally we should create more tests, we could use something like https://github.com/dubzzz/fast-check to generate many test cases
@@ -8,5 +8,20 @@ describe("datetimeToISO8601", () => {
 		// months in ecmascript starts from zero :P
 		const date = new Date(2024, 7, 1, 20, 0, 0);
 		expect(datetimeToISO8601(date)).toBe("2024-08-01 20:00:00");
+	});
+});
+
+describe("restrictionsToBits", () => {
+	// ideally we should create more tests, we could use something like https://github.com/dubzzz/fast-check to generate many test cases
+	test("serializes correctly zero restrictions", () => {
+		expect(restrictionsToBits([])).toBe(0);
+	});
+
+	test("serializes correctly 1 restriction", () => {
+		expect(restrictionsToBits([1])).toBe(1);
+	});
+
+	test("serializes correctly 2 restriction", () => {
+		expect(restrictionsToBits([1, 3])).toBe(5);
 	});
 });
--- utils.ts
+++ utils.ts
@@ -3,3 +3,12 @@ export const datetimeToISO8601 = (datetime: Date) => {
 	const [datePart, timePart] = datetime.toISOString().split("T");
 	return `${datePart} ${timePart.replace(/\.\d+Z/, "")}`;
 };
+
+export const restrictionsToBits = (restrictions: number[]): number => {
+	let result = 0;
+	// database ids starts at 1, to not waste bit 0 we always substract 1.
+	for (const r of restrictions) {
+		result |= 1 << (r - 1);
+	}
+	return result;
+};
