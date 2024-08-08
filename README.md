fc3520adb6d5612bcd2a699f06e04b6de69dafaa
Nico Gallinal <nicoabie@gmail.com>
2024-08-08 13:37:58 -0300

    add foreign keys

--- README.md
+++ README.md
@@ -20,4 +20,4 @@ To run:
 bun run index.ts
 ```
 
-This project was created using `bun init` in bun v1.1.21. [Bun](https://bun.sh) is a fast all-in-one JavaScript runtime.
+This project was created using `bun init` in bun v1.1.22. [Bun](https://bun.sh) is a fast all-in-one JavaScript runtime.
--- schema.sql
+++ schema.sql
@@ -22,7 +22,8 @@ DROP TABLE IF EXISTS tables;
 CREATE TABLE tables (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   restaurant_id INTEGER NOT NULL,
-  capacity INTEGER
+  capacity INTEGER,
+  CONSTRAINT fk_restaurant_id FOREIGN KEY (restaurant_id) REFERENCES restaurants (id)
 );
 
 -- reservations and diners_reservations will get huge over time, we can use datetime column in both tables to archive past records. daily or weekly process
@@ -36,7 +37,9 @@ CREATE TABLE reservations (
   -- it may be the case that we gave a bigger table, it is nice to store the actual capacity in case a smaller table gets freed and we can move the reservation to not waste places
   -- that process is OOS but we leave the door open for its implementation
   capacity INTEGER,
-  datetime DATETIME 
+  datetime DATETIME,
+  CONSTRAINT fk_restaurant_id FOREIGN KEY (restaurant_id) REFERENCES restaurants (id), 
+  CONSTRAINT fk_table_id FOREIGN KEY (table_id) REFERENCES tables (id) 
 );
 
 DROP TABLE IF EXISTS diners_reservations;
@@ -47,7 +50,9 @@ CREATE TABLE diners_reservations (
   -- this is the same datetime as in reservations
   -- we include it here to fastly check if any user has already a reservation at a given datetime
   -- and for archiving past records too
-  datetime DATETIME
+  datetime DATETIME,
+  CONSTRAINT fk_diner_id FOREIGN KEY (diner_id) REFERENCES diners (id), 
+  CONSTRAINT fk_reservation_id FOREIGN KEY (reservation_id) REFERENCES reservations (id)
 );
 
 DROP TABLE IF EXISTS restrictions;
